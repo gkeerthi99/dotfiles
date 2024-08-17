523b83ed46d3075f983f580e903557897249656a
Keerthi <gkeerthi1999@gmail.com>
2024-08-17 20:02:06 +0800

    Added mappings.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -13,8 +13,6 @@ map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "switch window right" }
 map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "switch window down" })
 map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "switch window up" })
 
-map("n", "<leader>`", "<C-^>", { desc = "switch to previous buffer" })
-
 -- Telescope keybindings
 map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
 map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
@@ -26,19 +24,33 @@ map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git
 map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
 map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
 map(
-  "n",
-  "<leader>ff",
-  "<cmd>lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '--glob', '!.git/*'} })<CR>",
-  { desc = "telescope find files" }
+	"n",
+	"<leader>ff",
+	"<cmd>lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '--glob', '!.git/*'} })<CR>",
+	{ desc = "telescope find files" }
 )
 map(
-  "n",
-  "<leader>fa",
-  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-  { desc = "telescope find all files" }
+	"n",
+	"<leader>fa",
+	"<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+	{ desc = "telescope find all files" }
 )
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "display code actions" })
 map("n", "<C-p>", "<cmd>Telescope projects<CR>", { desc = "find projects" })
+
+-- Buffer keybindings
+map("n", "<leader>`", "<C-^>", { desc = "switch to previous buffer" })
+map("n", "<leader>1", "<cmd>BufferGoto 1<CR>")
+map("n", "<leader>2", "<cmd>BufferGoto 2<CR>")
+map("n", "<leader>3", "<cmd>BufferGoto 3<CR>")
+map("n", "<leader>4", "<cmd>BufferGoto 4<CR>")
+map("n", "<leader>5", "<cmd>BufferGoto 5<CR>")
+map("n", "<leader>6", "<cmd>BufferGoto 6<CR>")
+map("n", "<leader>7", "<cmd>BufferGoto 7<CR>")
+map("n", "<leader>8", "<cmd>BufferGoto 8<CR>")
+map("n", "<leader>9", "<cmd>BufferGoto 9<CR>")
+map("n", "<leader>0", "<cmd>BufferLast<CR>", { desc = "switch to last buffer" })
+map("n", "<leader>c", "<Cmd>BufferWipeout<CR>", { desc = "close buffer" })
 
 -- Neotree keybindings
 map("n", "<C-b>", "<cmd>Neotree toggle<CR>", { desc = "toggle neotree" })
@@ -52,8 +64,8 @@ map("n", "<C-c>", "<cmd>ChatGPT<CR>", { desc = "Add breakpoint at line" })
 
 -- Format file on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.format()
-  end,
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
