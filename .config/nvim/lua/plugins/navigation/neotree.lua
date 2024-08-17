1f4df17ec61ea788141b648a3ce3030145016e82
Keerthi <gkeerthi1999@gmail.com>
2024-08-17 19:29:29 +0800

    Edited neotree.

--- .config/nvim/lua/plugins/navigation/neotree.lua
+++ .config/nvim/lua/plugins/navigation/neotree.lua
@@ -1,36 +1,40 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    "3rd/image.nvim",            -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  deactivate = function()
-    vim.cmd([[Neotree close]])
-  end,
-  init = function() end,
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-      },
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = true,
-      },
-    },
-    buffers = {
-      follow_current_file = {
-        enable = true,
-      },
-    },
-    window = {
-      position = "left",
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("BufEnter", {
+			once = true,
+			callback = function()
+				vim.cmd([[Neotree]])
+			end,
+		})
+	end,
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				show_hidden_count = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = true,
+			},
+		},
+		buffers = {
+			follow_current_file = {
+				enable = true,
+			},
+		},
+		window = {
+			position = "left",
+		},
+	},
 }
