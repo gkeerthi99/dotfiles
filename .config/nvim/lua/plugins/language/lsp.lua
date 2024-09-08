a008375411c1d9acc91902e1af18e87812f1ec07
Keerthi <gkeerthi1999@gmail.com>
2024-09-08 18:21:00 +0800

    Edited config.

--- .config/nvim/lua/plugins/language/lsp.lua
+++ .config/nvim/lua/plugins/language/lsp.lua
@@ -1,51 +1,51 @@
 local servers = { "biome", "cssls", "html", "htmx", "lua_ls", "pyright", "tailwindcss", "ts_ls" }
 
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {},
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = servers,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig/util")
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = servers,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local util = require("lspconfig/util")
 
-      local config = require("config.lsp_config")
-      local on_attach = config.on_attach
-      local capabilities = config.capabilities
+			local config = require("config.lsp_config")
+			local on_attach = config.on_attach
+			local capabilities = config.capabilities
 
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-      end
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end
 
-      lspconfig.gopls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        cmd = { "gopls" },
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-        settings = {
-          gopls = {
-            completeUnimported = true,
-          },
-        },
-      })
+			lspconfig.gopls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+				settings = {
+					gopls = {
+						completeUnimported = true,
+					},
+				},
+			})
 
-      lspconfig.clangd.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        cmd = { "clangd", "--header-insertion=iwyu" }, -- Auto include headers
-      })
-    end,
-  },
+			lspconfig.clangd.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { "clangd", "--header-insertion=iwyu" }, -- Auto include headers
+			})
+		end,
+	},
 }
