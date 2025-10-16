6712a35a1ecc57fdc05f8cc055c7b8085f271837
Keerthi Ganesh <gkeerthi1999@gmail.com>
2025-10-16 22:01:49 +0800

    Edited lsp config.

--- .config/nvim/lua/plugins/language/lsp.lua
+++ .config/nvim/lua/plugins/language/lsp.lua
@@ -1,90 +1,95 @@
-local servers = { "biome", "cssls", "gopls", "html", "htmx", "lua_ls", "pyright", "rust_analyzer", "tailwindcss", "templ",
-  "ts_ls" }
+local servers = {
+	"biome",
+	"cssls",
+	"glsl_analyzer",
+	"gopls",
+	"html",
+	"htmx",
+	"lua_ls",
+	"pyright",
+	"rust_analyzer",
+	"tailwindcss",
+	"templ",
+	"ts_ls",
+}
 
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = servers,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig/util")
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = servers,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local config = require("config.lsp_config")
+			local on_attach = config.on_attach
+			local capabilities = config.capabilities
 
-      local config = require("config.lsp_config")
-      local on_attach = config.on_attach
-      local capabilities = config.capabilities
+			vim.lsp.config("clangd", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { "clangd", "--header-insertion=iwyu" }, -- Auto include headers
+			})
+			vim.lsp.enable({ "clangd" })
 
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-      end
+			vim.lsp.config("gopls", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_markers = { "go.work", "go.mod", ".git" },
+				root_dir = nil,
+				settings = {
+					gopls = {
+						completeUnimported = true,
+					},
+				},
+			})
 
-      lspconfig.clangd.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        cmd = { "clangd", "--header-insertion=iwyu" }, -- Auto include headers
-      })
+			vim.lsp.config("glsl_analyzer", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
 
-      lspconfig.gopls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        cmd = { "gopls" },
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-        settings = {
-          gopls = {
-            completeUnimported = true,
-          },
-        },
-      })
+			vim.lsp.config("html", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				filetypes = { "html", "templ" },
+			})
 
-      lspconfig.html.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "html", "templ" },
-      })
+			vim.lsp.config("htmx", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				filetypes = { "html", "templ" },
+			})
 
-      lspconfig.htmx.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "html", "templ" },
-      })
+			vim.lsp.config("rust_analyzer", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				filetypes = { "rust" },
+				root_markers = { "Cargo.toml" },
+				root_dir = nil,
+				settings = {
+					["rust_analyzer"] = {
+						cargo = {
+							allFeatures = true,
+						},
+					},
+				},
+			})
 
-      lspconfig.rust_analyzer.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "rust" },
-        root_dir = util.root_pattern("Cargo.toml"),
-        settings = {
-          ['rust_analyzer'] = {
-            cargo = {
-              allFeatures = true,
-            }
-          }
-        }
-      })
-
-      lspconfig.tailwindcss.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "templ", "astro", "javascript", "typescript", "react" },
-        init_options = { userLanguages = { templ = "html" } },
-      })
-
-      lspconfig.templ.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-    end,
-  },
+			vim.lsp.config("tailwindcss", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+				init_options = { userLanguages = { templ = "html" } },
+			})
+		end,
+	},
 }
