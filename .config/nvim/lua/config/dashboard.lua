006b8b4a3fb2f04a6866ddda1a1d0efdd4bf7b53
Keerthi Ganesh <gkeerthi1999@gmail.com>
2026-02-06 22:13:49 +0800

    Edited dashboard.

--- .config/nvim/lua/config/dashboard.lua
+++ .config/nvim/lua/config/dashboard.lua
@@ -47,15 +47,15 @@ H@:         :HHHHHHHHHHHHHHHHHHX,    =@H
 -- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 -- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 --       ]],
-      keys = {
-        { icon = " ", key = "f", desc = "Find File", action = ":lua require('fff').find_files()" },
-        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-        { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    keys = {
+      { icon = " ", key = "f", desc = "Find File", action = ":lua require('fff').find_files()" },
+      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+      { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+      { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+      { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
       },
     },
     sections = {
@@ -63,9 +63,6 @@ H@:         :HHHHHHHHHHHHHHHHHHX,    =@H
       { section = "keys",   gap = 1, padding = 2 },
       { footer = 'THE TRUTH IS OUT THERE' },
     },
-    -- Monokai Classic
-    vim.api.nvim_set_hl(0, "SnacksDashboardKey", { fg = "#a6e22e", bold = true }),
-    vim.api.nvim_set_hl(0, "SnacksDashboardFooter", { fg = "#57584f", italic = true }),
   }
 end
 
