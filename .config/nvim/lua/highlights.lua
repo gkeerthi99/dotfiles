660977c6baeb820e293d66824864d2d44e2b9037
Keerthi Ganesh <gkeerthi1999@gmail.com>
2026-05-17 22:45:38 +0800

    Added custom highlights file

---
+++ .config/nvim/lua/highlights.lua
@@ -0,0 +1,5 @@
+-- Custom highlights
+vim.api.nvim_set_hl(0, "SnacksDashboardDesc", { link = "@string" })
+vim.api.nvim_set_hl(0, "SnacksDashboardKey", { link = "@function.call" })
+vim.api.nvim_set_hl(0, "SnacksDashboardFooter", { link = "@comment" })
+vim.api.nvim_set_hl(0, "SnacksDashboardIcon", { link = "@variable.member" })
